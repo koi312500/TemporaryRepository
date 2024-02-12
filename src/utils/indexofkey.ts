372a60723ba65be7690aa304aeba431ef543e854
루야 <159050727+Luya369@users.noreply.github.com>
2024-02-13 08:29:59 +0900

    Delete src/utils/indexofkey.ts

--- src/utils/indexofkey.ts
+++
@@ -1,8 +0,0 @@
-export function findIndexByKey(jsonArray, key: string): number {
-  for (let i = 0; i < jsonArray.length; i++) {
-    if (jsonArray[i].key === key) {
-      return i;
-    }
-  }
-  return -1; // 찾지 못한 경우
-}
